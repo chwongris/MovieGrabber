34b15e8a55a2a096802c3f16447bac19466c7d0e
Chris Wong <chwongris@gmail.com>
2013-04-02 01:25:52 -0400

    sorta works... but creating duplicates everytime you run the test

--- movies.rb
+++ movies.rb
@@ -1,18 +1,38 @@
-class Movie
+# db.execute("create table movies (id integer Primary Key, title varchar, year integer, director varchar, imdbRating float, plot varchar)")
 
+class Movie 
+  attr_accessor :title
+  attr_accessor :year
+  attr_accessor :director
+  attr_accessor :imdbRating
+  attr_accessor :plot
   # Add attr_accessors for the values you want to store...
 
+
   def self.get_film_info(name)    
+    @@db = SQLite3::Database.new("test/test.db")
     imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{name}")
     movie_info = JSON(imdb_data)
-    binding.pry
-
+# a = @@db.execute("select * from movies where title is '#{name}'")
+# binding.pry
+    # if  @@db.execute("select * from movies where title is '#{name}'").[ == []
     # Create a Movie object...
-    m = Movie.new
+      m = Movie.new
+      m.title = movie_info["Title"]
+      m.year = movie_info["Year"]
+      m.director = movie_info["Director"]
+      m.imdbRating= movie_info["imdbRating"]
+      m.plot= movie_info["Plot"]
+      
 
     # Fill in the attributes...
 
     # Store me in a SQLite3 database...    
+    sql = "Insert into movies (title, year, director, imdbRating, plot) values (?,?,?,?,?)"
+# binding.pry
+    @@db.execute(sql, m.title, m.year, m.director, m.imdbRating, m.plot)
+    # else
+    # end
   end
 
 end
--- test/person_test.rb
+++ test/person_test.rb
@@ -7,18 +7,22 @@ require_relative '../movies'
 
 class MovieGrabberTest < Test::Unit::TestCase
 
-  def setup
+ def setup
     @@db = SQLite3::Database.new("test/test.db")
   end
 
+
+
   def test_get_film_info_method
     Movie.get_film_info("jaws")
-
+    movies = []
     # Don't forget to create your movies table first, using sqlite3...
     movies = @@db.execute("select * from movies")
-    movies.length.should == 1
-    movies.first[1].should == "Jaws" # Note, this can change if you want/need
+    
+    assert_equal 1, movies.length
+    assert_equal "Jaws", movies.first[1] # Note, this can change if you want/need
     # Add other parts to your test here...
+
   end
 
   def teardown
